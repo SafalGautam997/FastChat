cf79ad8f560547885afcc2c73bb54738786b174b
SafalGautam977 <safalgautam977@gmail.com>
2024-12-16 14:59:16 +0545

    Livewire components connected and Buttons created

--- resources/views/livewire/chat/chat.blade.php
+++ resources/views/livewire/chat/chat.blade.php
@@ -1,15 +1,19 @@
-@extends('layouts.app')
-@section('content')
-    {{-- Replace with your layout path --}}
-    <div
-        class="hidden lg:flex fixed h-full flex bg-white border lg:shadow-sm overflow-hidden inset-0 lg:top-16 lg:inset-x-2 m-auto lg:h-[90%] rounded-t-lg">
-        <div class="hidden lg:flex relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border">
-            <livewire:chat.chat-list>
+<div
+    class=" fixed  h-full  flex bg-white border  lg:shadow-sm overflow-hidden inset-0 lg:top-16  lg:inset-x-2 m-auto lg:h-[90%] rounded-t-lg">
+
+    <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border">
+
+        <livewire:chat.chat-list>
+    </div>
+
+    <div class="hidden md:grid   w-full border-l h-full relative overflow-y-auto" style="contain:content">
+
+        <div class="m-auto text-center justify-center flex flex-col gap-3">
+
+            <h4 class="font-medium text-lg"> Choose a conversation to start chatting </h4>
+
         </div>
 
-        <div class="grid   w-full border-l h-full relative overflow-y-auto" style="contain:content">
-            <livewire:chat.chat-box>
-        </div>
-        {{-- Add your content here --}}
     </div>
-@endsection
+
+</div>
